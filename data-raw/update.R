ac05cd672fb81ac89e20ecea49fcaaeafe4593c3
maurolepore <maurolepore@gmail.com>
2018-11-17 15:10:44 -0500

    Move update to data-raw/ but provide a shortcut via update().

---
+++ data-raw/update.R
@@ -0,0 +1,30 @@
+update_dependencies <- function() source("data-raw/dependencies.R")
+
+update_build <- function() {
+  fs::file_delete(fgeo.install:::src_paths())
+
+  source("data-raw/build.R")
+
+  rename_filenames_by_installation_order()
+}
+
+rename_filenames_by_installation_order <- function() {
+  pattern_replacement <- c(
+    "fgeo.x_"    = "01-fgeo.x_",
+    "fgeo.base_" = "02-fgeo.base_",
+    "fgeo.ctfs_" = "03-fgeo.ctfs_",
+    "fgeo.tool_" = "04-fgeo.tool_",
+    "fgeo.map_"  = "05-fgeo.map_",
+    "fgeo_"      = "zz-fgeo_"
+  )
+
+  path <- fgeo.install:::src_paths()
+  new_path <- stringr::str_replace_all(path, pattern_replacement)
+  fs::file_move(path, new_path)
+}
+
+
+# Run -------------------------------------------------------------------
+
+update_dependencies()
+update_build()
