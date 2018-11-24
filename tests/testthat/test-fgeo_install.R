6ef6baaed08515f3007fbf9d68378a24d7cd50fc
maurolepore <maurolepore@gmail.com>
2018-11-24 13:31:59 -0500

    Refactor install_these() to not install from CRAN.

--- tests/testthat/test-fgeo_install.R
+++ tests/testthat/test-fgeo_install.R
@@ -4,14 +4,6 @@ test_that("errs with informative message", {
   expect_error(install_these(), "is missing.*no default")
 })
 
-test_that("Installs multiple packages from CRAN", {
-  pkgs <- c("io", "clipr")
-  remove.packages(pkgs)
-  out <- capture_output(install_these(pkgs))
-  expect_true(grepl("package.*io.*successfully unpacked", out))
-  expect_true(grepl("package.*clipr.*successfully unpacked", out))
-})
-
 test_that("Installs multiple packages from a local source", {
   tmp <- tempfile()
   fs::dir_create(tmp)
